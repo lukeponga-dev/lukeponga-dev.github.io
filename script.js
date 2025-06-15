be6f954453f87745df72f8d40b5c24bc84068c06
Luke Ponga <51413507+lukeponga-dev@users.noreply.github.com>
2025-06-16 06:29:23 +1200

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ const navLinks = document.querySelectorAll('nav ul li a');
 
 navLinks.forEach(link => {
   link.addEventListener('click', e => {
-    e.preventDefault(); // Prevent default anchor behavior
-
-    const targetId = link.getAttribute('href').substring(1); // Get the target ID
-    const targetSection = document.getElementById(targetId); // Find the section
-
-    // Scroll smoothly to the target
-    targetSection.scrollIntoView({ behavior: 'smooth' });
+    e.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' });
+    }
   });
 });
